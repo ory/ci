85ebf9a209a94d9908ed26295227caed00c1012a
aeneasr <aeneas@ory.sh>
2020-04-06 13:12:18 +0200

    fix: use right gopath/bin

--- src/scripts/install/swagger.sh
+++ src/scripts/install/swagger.sh
@@ -4,7 +4,7 @@ set -Eeuox pipefail
 
 version=v0.23.0
 
-path=$GOPATH
+path=$GOPATH/bin
 
 download_url=$(curl -s https://api.github.com/repos/go-swagger/go-swagger/releases | \
   jq -r '.[] | select(.tag_name=="'"${version}"'") | .assets[] | select(.name | contains("'"$(uname | tr '[:upper:]' '[:lower:]')"'_amd64")) | .browser_download_url')
