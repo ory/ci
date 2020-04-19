542ffb255aee82226e5a5d95a6c85693c831e835
aeneasr <aeneas@ory.sh>
2020-04-19 13:48:22 +0200

    fix: improve docker hub default

--- src/scripts/releaser/test.sh
+++ src/scripts/releaser/test.sh
@@ -2,6 +2,8 @@
 
 set -Eeuox pipefail
 
+hub=${DOCKER_HUB_NAME:-oryd}
+
 curl -sSfL \
   "https://raw.githubusercontent.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/master/install.sh" \
   | sh -s "${CIRCLE_TAG}"
@@ -9,7 +11,6 @@ curl -sSfL \
 "./bin/${CIRCLE_PROJECT_REPONAME}" help
 "./bin/${CIRCLE_PROJECT_REPONAME}" version | grep -q "${CIRCLE_TAG}" || exit 1
 
-hub=${DOCKER_HUB_NAME:-oryd}
 docker run --rm \
   "${hub}/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_TAG}" help
 docker run --rm \
