e1efc43bbc203a461bac3538855e92a7bf57c4ad
aeneasr <aeneas@ory.sh>
2020-06-24 09:02:39 +0200

    fix: bump nancy

---
+++ src/scripts/nancy/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -Eeuox pipefail
+
+curl -L -o $GOPATH/bin/nancy https://github.com/sonatype-nexus-community/nancy/releases/download/v0.3.1/nancy-linux.amd64-v0.3.1 && chmod +x $GOPATH/bin/nancy
+
+go mod list -m all | nancy -quiet -exclude-vulnerability fc041c7e-0c64-4b74-991e-64196a704ace,2bfd5f1a-e9b5-4a04-b4e8-15091e145845,40365e0e-8b3c-4252-8124-dbbec0a45dfe
