61b6dbdf9bfb9780dce5328305453db3ea0cf5b5
zepatrik <patrik@ory.sh>
2020-08-04 14:41:59 +0200

    push aswell

--- src/scripts/releaser/render-version-schema.sh
+++ src/scripts/releaser/render-version-schema.sh
@@ -6,3 +6,4 @@ bash <(curl https://raw.githubusercontent.com/ory/cli/master/install.sh) -b $GOP
 
 ory dev schema render-version $CIRCLE_TAG
 git commit -A -m "autogen: update version.schema.json to include new release"
+git push
