389fc363006b05bd06cfbf1c5ac2442b7661c37a
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-08-04 17:10:58 +0200

    Update src/scripts/releaser/render-version-schema.sh

--- src/scripts/releaser/render-version-schema.sh
+++ src/scripts/releaser/render-version-schema.sh
@@ -5,5 +5,5 @@ set -Eeuox pipefail
 bash <(curl https://raw.githubusercontent.com/ory/cli/master/install.sh) -b $GOPATH/bin
 
 ory dev schema render-version $CIRCLE_TAG
-git commit -A -m "autogen: update version.schema.json to include new release"
+git commit -A -m "autogen: add $CIRCLE_TAG to version.schema.json"
 git push
