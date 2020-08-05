fb2c33ecce42b186122b1651dc67394ad5ba831a
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-08-05 23:45:38 +0200

    fix: use github noreply email

--- src/scripts/install/git.sh
+++ src/scripts/install/git.sh
@@ -2,5 +2,5 @@
 
 set -Eeuox pipefail
 
-git config --global user.email "aeneas@ory.sh"
+git config --global user.email "3372410+aeneasr@users.noreply.github.com"
 git config --global user.name "aeneasr"
