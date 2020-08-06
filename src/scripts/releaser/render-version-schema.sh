77a7d14ff3b2394c5f1bbd7a5c13c40a1cb8a4ee
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-08-06 12:21:24 +0200

    fix: set up git in render-version-schema

--- src/scripts/releaser/render-version-schema.sh
+++ src/scripts/releaser/render-version-schema.sh
@@ -2,6 +2,7 @@
 
 set -Eeuox pipefail
 
+bash <(curl -s https://raw.githubusercontent.com/ory/ci/master/src/scripts/install/git.sh)
 bash <(curl https://raw.githubusercontent.com/ory/cli/master/install.sh) -b $GOPATH/bin
 
 ory dev schema render-version $CIRCLE_TAG
