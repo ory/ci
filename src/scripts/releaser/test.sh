e534499f3ca39d1677ce7c228a898d31a42d2345
zepatrik <patrik@ory.sh>
2020-08-06 16:17:43 +0200

    fix: map "cli" repo to "ory" binary name

--- src/scripts/releaser/test.sh
+++ src/scripts/releaser/test.sh
@@ -4,12 +4,17 @@ set -Eeuox pipefail
 
 hub=${DOCKER_HUB_NAME:-oryd}
 
+BINARY_NAME=$CIRCLE_PROJECT_REPONAME
+if [[ $CIRCLE_PROJECT_REPONAME = "cli" ]] ;then
+  BINARY_NAME="ory"
+fi
+
 curl -sSfL \
   "https://raw.githubusercontent.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/master/install.sh" \
   | sh -s "${CIRCLE_TAG}"
 
-"./bin/${CIRCLE_PROJECT_REPONAME}" help
-"./bin/${CIRCLE_PROJECT_REPONAME}" version | grep -q "${CIRCLE_TAG}" || exit 1
+"./bin/${BINARY_NAME}" help
+"./bin/${BINARY_NAME}" version | grep -q "${CIRCLE_TAG}" || exit 1
 
 docker run --rm \
   "${hub}/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_TAG}" help
