d97765330387043d1a867bae4c94161f97159652
zepatrik <patrik@ory.sh>
2020-08-31 17:36:21 +0200

    fix: provide project name to render version schema via argument

--- src/scripts/releaser/render-version-schema.sh
+++ src/scripts/releaser/render-version-schema.sh
@@ -5,6 +5,6 @@ set -Eeuox pipefail
 bash <(curl -s https://raw.githubusercontent.com/ory/ci/master/src/scripts/install/git.sh)
 bash <(curl https://raw.githubusercontent.com/ory/cli/master/install.sh) -b $GOPATH/bin
 
-ory dev schema render-version $CIRCLE_TAG
+ory dev schema render-version $CIRCLE_PROJECT_REPONAME $CIRCLE_TAG
 git commit -a -m "autogen: add $CIRCLE_TAG to version.schema.json"
 git push --set-upstream origin $CIRCLE_BRANCH
