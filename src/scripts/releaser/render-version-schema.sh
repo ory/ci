7a805553a01d9879d5fe3211c082b314946b3a67
zepatrik <patrik@ory.sh>
2020-10-05 12:25:18 +0200

    fix: improve pushing for goreleaser/render-version-schema Closes #33

--- src/scripts/releaser/render-version-schema.sh
+++ src/scripts/releaser/render-version-schema.sh
@@ -5,7 +5,10 @@ set -Eeuox pipefail
 bash <(curl -s https://raw.githubusercontent.com/ory/ci/master/src/scripts/install/git.sh)
 bash <(curl https://raw.githubusercontent.com/ory/cli/master/install.sh) -b $GOPATH/bin
 
+git fetch origin
+git checkout -b master origin/master
+
 ory dev schema render-version $CIRCLE_PROJECT_REPONAME $CIRCLE_TAG
+
 git commit -a -m "autogen: add $CIRCLE_TAG to version.schema.json"
-# pushing detached HEAD to master
-git push origin HEAD:master
+git push origin master
