31f1f1f63be039fbe72b0231ef905b04826f95ee
Patrik <zepatrik@users.noreply.github.com>
2020-10-05 16:51:29 +0200

    fix: use suggested git flow
    Co-authored-by: hackerman <3372410+aeneasr@users.noreply.github.com>

--- src/scripts/releaser/render-version-schema.sh
+++ src/scripts/releaser/render-version-schema.sh
@@ -5,10 +5,13 @@ set -Eeuox pipefail
 bash <(curl -s https://raw.githubusercontent.com/ory/ci/master/src/scripts/install/git.sh)
 bash <(curl https://raw.githubusercontent.com/ory/cli/master/install.sh) -b $GOPATH/bin
 
-git fetch origin
-git checkout -b master origin/master
-
 ory dev schema render-version $CIRCLE_PROJECT_REPONAME $CIRCLE_TAG
 
+branch="$(date +%s)"
+git fetch origin
+git stash
+git checkout -b "$branch"
+git stash pop
 git commit -a -m "autogen: add $CIRCLE_TAG to version.schema.json"
-git push origin master
+git pull origin master --rebase
+git push origin HEAD:master
