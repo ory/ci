bf6ad9ba4ffe08d6f0d49fb675fb9484318df410
Patrik <zepatrik@users.noreply.github.com>
2020-10-06 22:56:03 +0200

    fix: install prettier version from package.json (#41)

--- src/scripts/install/prettier.sh
+++ src/scripts/install/prettier.sh
@@ -4,5 +4,5 @@ set -Eeuox pipefail
 
 mkdir -p node_modules/ory-prettier-styles
 tar -xf "$(npm pack ory-prettier-styles)" -C node_modules/ory-prettier-styles --strip-components=1
-sudo npm i -g prettier
+sudo npm i -g "prettier@$(jq -r '.devDependencies.prettier' package.json)"
 rm ory-prettier-styles-*.tgz
