102d9c5b7719ee9b711808ce0bcf97d26932afa2
zepatrik <zepatrik@users.noreply.github.com>
2021-03-08 12:43:02 +0100

    fix: nancy binary URL

--- src/scripts/nancy/run.sh
+++ src/scripts/nancy/run.sh
@@ -2,6 +2,6 @@
 
 set -Eeuox pipefail
 
-curl -L -o $GOPATH/bin/nancy https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.15/nancy-linux.amd64-v1.0.5 && chmod +x $GOPATH/bin/nancy
+curl -L -o $GOPATH/bin/nancy https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.15/nancy-v1.0.15-linux-amd64 && chmod +x $GOPATH/bin/nancy
 
 go list -m all | nancy sleuth -q
