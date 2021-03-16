b43f99516100934bd6039d5f7a980a2000dce957
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-03-16 12:54:48 +0100

    fix: properly add rendered version schema

--- src/scripts/releaser/render-version-schema.sh
+++ src/scripts/releaser/render-version-schema.sh
@@ -9,9 +9,10 @@ ory dev schema render-version $CIRCLE_PROJECT_REPONAME $CIRCLE_TAG
 
 branch="$(date +%s)"
 git fetch origin
-git stash || true
+git stash || true
 git checkout -b "$branch"
 git stash pop || true
-git commit -a -m "autogen: add $CIRCLE_TAG to version.schema.json"
-git pull origin master --rebase
-git push origin HEAD:master
+git add -A
+git commit -a -m "autogen: add $CIRCLE_TAG to version.schema.json" || true
+git pull origin master --rebase || true
+git push origin HEAD:master || true
