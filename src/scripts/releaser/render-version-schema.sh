87db09d23de974ebf9b25f7bdebe4d349f60da48
zepatrik <zepatrik@users.noreply.github.com>
2021-03-25 19:39:57 +0100

    fix(releaser): add config schema path parameter

--- src/scripts/releaser/render-version-schema.sh
+++ src/scripts/releaser/render-version-schema.sh
@@ -5,7 +5,7 @@ set -Eeuox pipefail
 bash <(curl -s https://raw.githubusercontent.com/ory/ci/master/src/scripts/install/git.sh)
 bash <(curl https://raw.githubusercontent.com/ory/cli/master/install.sh) -b $GOPATH/bin
 
-ory dev schema render-version $CIRCLE_PROJECT_REPONAME $CIRCLE_TAG
+ory dev schema render-version $CIRCLE_PROJECT_REPONAME $CIRCLE_TAG $SCHEMA_PATH
 
 branch="$(date +%s)"
 git fetch origin
