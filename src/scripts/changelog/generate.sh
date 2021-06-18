0322b589ffa77e5427ed6d6583601f167f030901
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-06-18 10:20:08 +0200

    feat: copy changelog to docs navigation
    Closes https://github.com/ory/kratos/issues/1443

--- src/scripts/changelog/generate.sh
+++ src/scripts/changelog/generate.sh
@@ -23,6 +23,21 @@ git clone git@github.com:ory/changelog.git "$preset"
 
 npx conventional-changelog-cli --config "$preset/index.js" -r 0 -u -o CHANGELOG.md
 
+# If docs/docs exists, copy the changelog there.
+if [ -d "docs/docs" ]; then
+  cat <<EOT >> docs/docs/CHANGELOG.md
+---
+id: changelog
+title: Changelog
+custom_edit_url: null
+---
+
+EOT
+  cat CHANGELOG.md >> docs/docs/CHANGELOG.md
+fi
+
+# Adding a table of contents and other things really only makes sense
+# for the CHANGELOG in the root repository.
 npx doctoc CHANGELOG.md
 
 sed -i "s/\*\*Table of Contents.*/**Table of Contents**/" CHANGELOG.md
@@ -39,14 +54,16 @@ t=$(mktemp)
 printf "# Changelog\n\n" | cat - CHANGELOG.md > "$t" && mv "$t" CHANGELOG.md
 
 if [ -z ${isRelease+x} ]; then
-  git add -A
-  (git commit -m "$COMMIT_MESSAGE" -- CHANGELOG.md && git pull -ff && git push origin HEAD:$CIRCLE_BRANCH) || true
+  git add -A CHANGELOG.md
+  [ -d "docs/docs" ] && git add docs/docs/CHANGELOG.md
+  (git commit -m "$COMMIT_MESSAGE" && git pull -ff && git push origin HEAD:$CIRCLE_BRANCH) || true
 else
-  git add -A
+  git add -A CHANGELOG.md
+  [ -d "docs/docs" ] && git add docs/docs/CHANGELOG.md
   git fetch origin
   git stash
   git checkout -b "changelog-$(date +"%m-%d-%Y")" origin/master
   git pull -ff
   git stash pop
-  (git commit -m "$COMMIT_MESSAGE" -- CHANGELOG.md && git pull -ff && git push origin HEAD:master) || true
+  (git commit -m "$COMMIT_MESSAGE" && git pull -ff && git push origin HEAD:master) || true
 fi
