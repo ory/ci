be2fed71732c14150551dda754018088ce002755
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-06-20 14:25:57 +0200

    fix: improve github release changelog

--- src/scripts/releaser/release.sh
+++ src/scripts/releaser/release.sh
@@ -11,7 +11,6 @@ else
   echo "package.json exists and needs not be written"
 fi
 
-changelog=$(mktemp)
 notes=$(mktemp)
 preset=$(mktemp -d)
 
@@ -19,11 +18,13 @@ npm --no-git-tag-version version "$CIRCLE_TAG"
 git clone git@github.com:ory/changelog.git "$preset"
 (cd "$preset"; npm i)
 
-git tag -l --format='%(contents)' "$CIRCLE_TAG" > "$notes"
-npx conventional-changelog-cli@v2.0.34 --config "$preset/index.js" -r 2 -o "$changelog"
+npx conventional-changelog-cli@v2.1.1 --config "$preset/index.js" -r 2 -o "$notes"
 
-printf "\n\n" >> "$notes"
-cat "$changelog" >> "$notes"
+# Replace all h1 headings from the changelog.
+sed '/^# /d' "$notes" | tee "$notes"
+
+# Format it
+npx prettier -w "$notes"
 
 git reset --hard HEAD
 goreleaser release --release-header <(cat "$notes") --rm-dist --timeout 60m --parallelism 1
