7c4dcd2f48806e4b03afb76fd2dfcabe7cedddc8
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-11-06 13:10:21 +0100

    fix: install as goreleaser

--- src/scripts/releaser/install.sh
+++ src/scripts/releaser/install.sh
@@ -5,5 +5,5 @@ set -Eeuox pipefail
 curl -Lo "goreleaser-pro_Linux_x86_64.tar.gz" "https://github.com/goreleaser/goreleaser-pro/releases/download/${2}-pro/goreleaser-pro_Linux_x86_64.tar.gz"
 mkdir -p goreleaser-pro_Linux_x86_64
 tar -xvf goreleaser-pro_Linux_x86_64.tar.gz -C goreleaser-pro_Linux_x86_64
-mv goreleaser-pro_Linux_x86_64/goreleaser "$1/goreleaser-pro"
+mv goreleaser-pro_Linux_x86_64/goreleaser "$1/goreleaser"
 rm -rf goreleaser-pro_Linux_x86_64.* goreleaser_Linux_x86_64/
