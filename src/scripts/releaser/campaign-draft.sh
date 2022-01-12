8e92241b4bbfb4f84347480e4c14088fa538a917
Anirudh Oppiliappan <x@icyphox.sh>
2022-01-12 11:42:15 +0530

    ci: add newsletter action

--- src/scripts/releaser/campaign-draft.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -Eeuox pipefail
-
-bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -b $GOPATH/bin ory
-
-ory dev release notify draft --segment "${MAILCHIMP_SEGMENT_ID}" "${MAILCHIMP_LIST_ID}"
-
-ory dev release notify send --dry "${MAILCHIMP_LIST_ID}"
--- src/scripts/releaser/campaign-send.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -Eeuox pipefail
-
-bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -b $GOPATH/bin ory
-
-ory dev release notify send "${MAILCHIMP_LIST_ID}"
