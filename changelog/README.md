3cac8257619ac12a0fc9b140d0963bef5f891e6c
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-02 17:52:15 -0500

    Developer-friendly changelog action

---
+++ changelog/README.md
@@ -0,0 +1,12 @@
+# "Create CHANGELOG.md" GitHub Action
+
+This GitHub Action generates, formats, and commits CHANGELOG.md files.
+
+### Development
+
+Run the business logic on your machine, `cd` into the repository that you want
+to generate changelogs for. Example: https://github.com/ory/hydra. Then run:
+
+```sh
+bash <path to create_changelog.sh in this repo>
+```
---
+++ changelog/create_changelog.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo
+echo "VERIFY THAT THE CHANGELOG IS NOT ALREADY GENERATED ..."
+if [[ "$(git show -s --format=%B | head -n 1)" == "autogen"* ]]; then
+	exit 0
+fi
+
+echo
+echo "FETCH GIT METADATA ..."
+git fetch origin +refs/tags/*:refs/tags/*
+
+echo
+echo "INSTALL TOOLS ..."
+npm i --no-save conventional-changelog-cli@v2.1.1 doctoc@v1.4.0 prettier@2.7.1
+git clone https://github.com/ory/changelog.git
+(cd changelog && npm ci)
+
+echo
+echo "GENERATE THE CHANGELOG ..."
+node_modules/.bin/conventional-changelog --config "changelog/index.js" -r 0 -u -o CHANGELOG.md
+node_modules/.bin/doctoc CHANGELOG.md
+sed -i "s/\*\*Table of Contents.*/**Table of Contents**/" CHANGELOG.md
+sed -i "s/\*This Change Log was.*/This Change Log was automatically generated/" CHANGELOG.md
+t=$(mktemp)
+printf "# Changelog\n\n" | cat - CHANGELOG.md >"$t" && mv "$t" CHANGELOG.md
+
+echo
+echo "FORMAT ..."
+# NOTE: need to format twice because of https://github.com/prettier/prettier/issues/13213
+node_modules/.bin/prettier --write CHANGELOG.md
+node_modules/.bin/prettier --write CHANGELOG.md
+node_modules/.bin/prettier --check CHANGELOG.md
+
+echo
+echo "CLEANUP ..."
+rm -rf changelog/
