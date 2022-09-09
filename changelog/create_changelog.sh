b402616f5d7d375f2c8b3f3e3ba505339e44ff18
Kevin Goslar <kevin.goslar@gmail.com>
2022-09-09 08:50:05 -0500

    create_changelog.sh

--- changelog/create_changelog.sh
+++ changelog/create_changelog.sh
@@ -16,14 +16,13 @@ git fetch origin +refs/tags/*:refs/tags/*
 
 echo
 echo "INSTALL TOOLS ..."
-npm i --no-save conventional-changelog-cli@v2.1.1 doctoc@v1.4.0 prettier@2.7.1
 git clone https://github.com/ory/changelog.git
 (cd changelog && npm ci)
 
 echo
 echo "GENERATE THE CHANGELOG ..."
-node_modules/.bin/conventional-changelog --config "changelog/index.js" -r 0 -u -o CHANGELOG.md
-node_modules/.bin/doctoc CHANGELOG.md
+npx --yes conventional-changelog@2.1.1 --config "changelog/index.js" -r 0 -u -o CHANGELOG.md
+npx --yes doctoc@1.4.0 CHANGELOG.md
 sed -i "s/\*\*Table of Contents.*/**Table of Contents**/" CHANGELOG.md
 sed -i "s/\*This Change Log was.*/This Change Log was automatically generated/" CHANGELOG.md
 t=$(mktemp)
@@ -32,9 +31,9 @@ printf "# Changelog\n\n" | cat - CHANGELOG.md >"$t" && mv "$t" CHANGELOG.md
 echo
 echo "FORMAT ..."
 # NOTE: need to format twice because of https://github.com/prettier/prettier/issues/13213
-node_modules/.bin/prettier --write CHANGELOG.md
-node_modules/.bin/prettier --write CHANGELOG.md
-node_modules/.bin/prettier --check CHANGELOG.md
+npx --yes prettier@2.7.1 --write CHANGELOG.md
+npx --yes prettier@2.7.1 --write CHANGELOG.md
+npx --yes prettier@2.7.1 --check CHANGELOG.md
 
 echo
 echo "CLEANUP ..."
