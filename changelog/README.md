1ffae4b5a5053329d8e6de0f9a511912f6ae90eb
Kevin Goslar <kevin.goslar@gmail.com>
2022-09-12 03:10:25 -0500

    fix: prettier bug + testability (#100)

---
+++ changelog/README.md
@@ -0,0 +1,13 @@
+# "Create CHANGELOG.md" GitHub Action
+
+This GitHub Action generates, formats, and commits CHANGELOG.md files.
+
+### Development
+
+To test this script by generating the changelog for a repository on your dev
+machine:
+
+- clone the repository for which to make a changelog
+- `cd` into its directory
+- run `bash create_changelog.sh` (adjust the path to point to the
+  `create_changelog.sh` file in this repo)
---
+++ changelog/create_changelog.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+
+# This script updates CHANGELOG.md.
+# It does commit the changes since that doesn't make sense during development.
+# This needs to happen separately on CI.
+
+echo
+echo "VERIFY THAT THE CHANGELOG IS NOT ALREADY GENERATED ..."
+if [[ "$(git show -s --format=%B | head -n 1)" == "autogen"* ]]; then
+	exit 0
+fi
+
+echo
+echo "FETCH GIT METADATA ..."
+git fetch origin +refs/tags/*:refs/tags/*
+
+echo
+echo "INSTALL TOOLS ..."
+rm -rf changelog
+git clone https://github.com/ory/changelog.git
+(cd changelog && npm ci)
+
+echo
+echo "GENERATE THE CHANGELOG ..."
+npx --yes conventional-changelog-cli@2.1.1 --config "changelog/index.js" -r 0 -u -o CHANGELOG.md
+npx --yes doctoc@1.4.0 CHANGELOG.md
+sed -i "s/\*\*Table of Contents.*/**Table of Contents**/" CHANGELOG.md
+sed -i "s/\*This Change Log was.*/This Change Log was automatically generated/" CHANGELOG.md
+t=$(mktemp)
+printf "# Changelog\n\n" | cat - CHANGELOG.md >"$t" && mv "$t" CHANGELOG.md
+
+echo
+echo "FORMAT THE CHANGELOG ..."
+# NOTE: need to format twice because of https://github.com/prettier/prettier/issues/13213
+npx --yes prettier@2.7.1 --write CHANGELOG.md
+npx --yes prettier@2.7.1 --write CHANGELOG.md
+npx --yes prettier@2.7.1 --check CHANGELOG.md
+
+echo
+echo "CLEANUP ..."
+rm -rf changelog/
