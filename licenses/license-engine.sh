0d00ac5b37bcb2e66d6b97ebf7c4a0e19d349ffe
Kevin Goslar <kevin.goslar@gmail.com>
2022-10-28 14:37:32 -0500

    feat: pre-approve Keto

--- licenses/license-engine.sh
+++ licenses/license-engine.sh
@@ -37,10 +37,11 @@ ALLOWED_LICENSES=(
 # These modules don't work with the current license checkers
 # and have been manually verified to have a compatible license (regex format).
 APPROVED_MODULES=(
-	'https://github.com/ory-corp/cloud/'                # Ory IP
-	'github.com/ory/kratos-client-go'                   # Apache-2.0
-	'github.com/ory/hydra-client-go'                    # Apache-2.0
-	'github.com/gobuffalo/github_flavored_markdown'     # MIT
+	'https://github.com/ory-corp/cloud/'            # Ory IP
+	'github.com/ory/kratos-client-go'               # Apache-2.0
+	'github.com/ory/hydra-client-go'                # Apache-2.0
+	'github.com/gobuffalo/github_flavored_markdown' # MIT
+	'github.com/ory/keto/.*'
 	'buffers@0.1.1'                                     # MIT: original source at http://github.com/substack/node-bufferlist is deleted but a fork at https://github.com/pkrumins/node-bufferlist/blob/master/LICENSE contains the original license by the original author (James Halliday)
 	'https://github.com/iconify/iconify/packages/react' # MIT: license is in root of monorepo at https://github.com/iconify/iconify/blob/main/license.txt
 	'github.com/gobuffalo/.*'                           # MIT: license is in root of monorepo at https://github.com/gobuffalo/github_flavored_markdown/blob/main/LICENSE
