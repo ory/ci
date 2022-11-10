32c2e143ce853bb5e2283a946f7acd5c42c38ec6
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-10 11:02:20 -0600

    chore: enforce full Git history for authors (#143)

--- authors/authors.sh
+++ authors/authors.sh
@@ -25,6 +25,12 @@ if [ -z "$PRODUCT" ]; then
 	exit 1
 fi
 
+# verify complete Git history
+if [ "$(git rev-parse --is-shallow-repository)" != "false" ]; then
+	echo "I need the full Git history in this repo"
+	exit 1
+fi
+
 # determine all authors from the Git history
 authors=$(git log --pretty=format:"%aN <%aE>" | sort | uniq)
 
